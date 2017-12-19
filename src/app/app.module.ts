768996e31b8c620c2a3924e6104cba99a0d1da63
SukritMatthanaphaisarn <sukrit@acommerce.asia>
2017-12-19 21:53:20 +0700

    create new comp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ImageViewerComponent } from './image-viewer/image-viewer.component';
+import { WeaponViewerComponent } from './weapon-viewer/weapon-viewer.component';
+import { SideViewerComponent } from './side-viewer/side-viewer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImageViewerComponent,
+    WeaponViewerComponent,
+    SideViewerComponent
   ],
   imports: [
     BrowserModule
